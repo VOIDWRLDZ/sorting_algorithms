bca29048d4e7ee9e641c30bfaf971f9d4df87123
Outtacosmos-ai <messghir4@gmail.com>
2024-10-05 14:12:25 +0100

    done ✅

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,8 +25,8 @@ void bubble_sort(int *array, size_t size)
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                swapped = 1;
                 print_array(array, size);
+                swapped = 1;
             }
         }
         if (swapped == 0)
