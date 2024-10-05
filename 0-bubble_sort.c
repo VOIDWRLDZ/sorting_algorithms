2c2d95d86b6a629f235046c394970ab60fa31716
Outtacosmos-ai <messghir4@gmail.com>
2024-10-05 13:50:52 +0100

    done ✅

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using a bubble
- * sort algorithm
- * @array: array of integers to be sorted
- * @size: amount of elements in array
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * @array: Pointer to the array of integers to sort
+ * @size: Size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
-	int temp;
-	bool swapped = true;
+    size_t i, n;
+    int temp;
+    int swapped;  /* Use int instead of bool */
 
-	if (!array || size < 2)
-		return;
+    if (!array || size < 2)
+        return;
 
-	n = size;
-	while (swapped)
-	{
-		swapped = false;
-		for (i = 1; i < n; i++)
-		{
-			if (array[i - 1] > array[i])
-			{
-				temp = array[i - 1];
-				array[i - 1] = array[i];
-				array[i] = temp;
-				swapped = true;
-				print_array(array, size);
-			}
-		}
-		/* Optimize by logically eliminating final swap from last loop */
-		n--;
-	}
+    n = size;
+    while (n > 1)
+    {
+        swapped = 0;  /* Set swapped to 0 (false) */
+        for (i = 0; i < n - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = 1;  /* Set swapped to 1 (true) */
+                print_array(array, size);  /* Print array after each swap */
+            }
+        }
+        n--;  /* Reduce n as the largest element is now sorted at the end */
+        if (!swapped)  /* If no elements were swapped, array is sorted */
+            break;
+    }
 }
