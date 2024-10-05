583cf25b5f362563ba6c9e2aa95afe5c86e3c449
Outtacosmos-ai <messghir4@gmail.com>
2024-10-05 13:31:24 +0100

    done ✅

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,41 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - sorting an array using bubble sort algorithm
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
  *
- * @array: the array to sort
- * @size: the size of the array
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+    size_t i, n;
+    int temp;
+    int swapped;
 
-	if (size < 2 || !array)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (array[j] < array[j - 1])
-			{
-				temp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = temp;
-				print_array(array, size);
-			}
-		}
-	}
+    for (n = size; n > 1; n--)
+    {
+        swapped = 0;
+        for (i = 0; i < n - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                /* Swap elements */
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+
+                /* Print array after each swap */
+                print_array(array, size);
+
+                swapped = 1;
+            }
+        }
+        /* If no elements were swapped, the array is already sorted */
+        if (!swapped)
+            break;
+    }
 }
--- sort.h
+++ sort.h
@@ -3,13 +3,7 @@
 
 #include <stddef.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+/* Data structure for doubly linked list node */
 typedef struct listint_s
 {
     const int n;
@@ -18,8 +12,8 @@ typedef struct listint_s
 } listint_t;
 
 /* Function prototypes */
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
