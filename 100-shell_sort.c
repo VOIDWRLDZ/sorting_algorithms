d6e5c77a76d87fa8e8beb0eb435b14e9cd9631e8
Outtacosmos-ai <messghir4@gmail.com>
2024-10-05 15:08:53 +0100

    done ✅

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,39 +1,27 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using Shell sort
- * @array: The array to be sorted
- * @size: Number of elements in the array
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, j;
-	int temp;
+	int gap = 1, i, j, tmp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	/* Calculate the largest gap using Knuth sequence */
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	while (gap > 0)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < (int)size; i += 1)
 		{
-			temp = array[i];
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-			{
+			tmp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
 				array[j] = array[j - gap];
-			}
-			array[j] = temp;
+
+			array[j] = tmp;
 		}
-
-		/* Print array after decreasing the interval */
 		print_array(array, size);
-
-		/* Calculate the next gap */
-		gap = (gap - 1) / 3;
 	}
 }
